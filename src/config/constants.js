e625d6cd4668435f0d52be9a56d13626f2de889c
Santiago Paiva <santiago.rosa991412@gmail.com>
2025-12-13 14:30:00 -0300

    config: adiciona configuração da API

---
+++ src/config/constants.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'http://agent-weathermap-env-env.eba-6pzgqekp.us-east-2.elasticbeanstalk.com/api/weather';
+export const APPID = 'AgentWeather2024_a8f3b9c1d7e2f5g6h4i9j0k1l2m3n4o5p6';
